076167c99cb980a3c6ac58a437325621e6d11ccf
Cihat Solak <cihatsolak@hotmail.com>
2020-10-28 16:57:16 +0300

    startup düzenlendi.

--- AspNetCoreIdentityCourse/MemberShipSystem/Startup.cs
+++ AspNetCoreIdentityCourse/MemberShipSystem/Startup.cs
@@ -4,9 +4,10 @@ using MemberShip.Web.Requirements;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using static MemberShip.Web.Tools.Constants.IdentityConstants;
+using Microsoft.Extensions.Hosting;
 
 namespace MemberShipSystem
 {
@@ -36,15 +37,22 @@ namespace MemberShipSystem
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseSession();
-            app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseRouting();
+
             app.UseAuthentication();
             app.UseAuthorization();
+
             app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("default", "{controller=Security}/{action=SignIn}/{id?}");
