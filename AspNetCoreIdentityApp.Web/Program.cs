06be451007ffd231bb7dc5417cc044c9aa60109a
Cihat Solak <cihatsolak@hotmail.com>
2023-02-19 14:02:29 +0300

    add first migration for web app

--- AspNetCoreIdentityApp.Web/Program.cs
+++ AspNetCoreIdentityApp.Web/Program.cs
@@ -2,8 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
+builder.Services.AddIdentity<AppUser, AppRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
--- AspNetCoreIdentityApp.Web/Usings.cs
+++ AspNetCoreIdentityApp.Web/Usings.cs
@@ -1,3 +1,4 @@
-﻿global using Microsoft.AspNetCore.Identity;
+﻿global using AspNetCoreIdentityApp.Web.Models;
+global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore;
