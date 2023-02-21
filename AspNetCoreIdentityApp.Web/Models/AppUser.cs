21b1d52889d9da02feb3ba8a177ba3771905679c
Cihat Solak <cihatsolak@hotmail.com>
2023-02-21 21:45:36 +0300

    add additional properties to app user class

--- AspNetCoreIdentityApp.Web/Models/AppUser.cs
+++ AspNetCoreIdentityApp.Web/Models/AppUser.cs
@@ -2,5 +2,9 @@
 {
     public class AppUser : IdentityUser
     {
+        public string City { get; set; }
+        public string Picture { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
     }
 }
