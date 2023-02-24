b0139b86d81138b7a77bb27a8e598be02065b57e
Cihat Solak <cihatsolak@hotmail.com>
2023-02-24 21:23:04 +0300

    user role update

--- AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
@@ -41,5 +41,35 @@
             TempData["SuccessMessage"] = "Rol oluşturulmuştur.";
             return RedirectToAction(nameof(RolesController.Index));
         }
+
+        
+        public async Task<IActionResult> RoleUpdate(string id)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(id);
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamamıştır.");
+            }
+
+            return View(new RoleUpdateViewModel() { Id = roleToUpdate.Id, Name = roleToUpdate!.Name! });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleUpdate(RoleUpdateViewModel request)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(request.Id);
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamamıştır.");
+            }
+
+            roleToUpdate.Name = request.Name;
+
+            await _roleManager.UpdateAsync(roleToUpdate);
+
+            ViewData["SuccessMessage"] = "Rol bilgisi güncellenmiştir";
+
+            return View();
+        }
     }
 }
